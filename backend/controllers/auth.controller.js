1e0ebf27eb00f97fc8e5c23d58fe29333012bc4b
arunasg28 <140942661+arunasg28@users.noreply.github.com>
2025-05-07 20:36:18 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -41,7 +41,7 @@ export const signup = async (req, res) => {
 		res.cookie("jwt-Mamcet", token, {
 			httpOnly: true, // prevent XSS attack
 			maxAge: 3 * 24 * 60 * 60 * 1000,
-			sameSite: "strict", // prevent CSRF attacks,
+			sameSite: "None", // prevent CSRF attacks,
 			secure: process.env.NODE_ENV === "production", // prevents man-in-the-middle attacks
 		});
 
@@ -75,7 +75,7 @@ export const login = async (req, res) => {
 		await res.cookie("jwt-Mamcet", token, {
 			httpOnly: true,
 			maxAge: 3 * 24 * 60 * 60 * 1000,
-			sameSite: "strict",
+			sameSite: "None",
 			secure: process.env.NODE_ENV === "production",
 		});
 
